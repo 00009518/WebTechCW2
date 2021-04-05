5ec400f5403e118547c57eaeb2508b5cdcfb3d69
00009518 <wiut00009518@gmail.com>
2021-04-05 12:22:42 +0500

    Built basic express web server

---
+++ app.js
@@ -0,0 +1,15 @@
+// To check the browser connectivity
+const express = require('express')
+const app = express()
+
+// getting the request
+app.get('/', (req, res) => {
+    res.send('Hello')
+    });
+    
+// listening  for requests
+app.listen(8000, err => {
+    if (err) console.log(err)
+    
+    console.log('App is listening on port  http://localhost:8000')
+  })
