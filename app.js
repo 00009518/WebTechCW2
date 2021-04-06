62ce26cbb0a1619d4f8bb31597eca1f37ce43475
00009518 <wiut00009518@gmail.com>
2021-04-06 08:49:23 +0500

    created all tasks page with styles, and corrected my other mistakes

--- app.js
+++ app.js
@@ -12,16 +12,23 @@ app.use(express.static('public'))
 // localhost:8000
 app.get('/', (req, res) => {
     res.render('index')
-    });
+});
 
 // create page
 app.get('/create_task', (req, res) => {
     res.render('create_task')
-    });
-    
+});
+
+
+const tasks = ['fewfgreger', 'eqERWfseawFWAfr']
+    // all tasks page
+app.get('/all_tasks', (req, res) => {
+    res.render('all_tasks', { tasks: tasks })
+});
+
 // listening  for requests
 app.listen(8000, err => {
     if (err) console.log(err)
-    
+
     console.log('App is listening on port  http://localhost:8000')
-  })
+})
